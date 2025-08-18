ed444913bfe86b339645967d753d8da52e0d5b64
Reginaldo Silva <reginaldo.silva@sap.ce.gov.br>
2025-08-18 16:27:54 -0300

    first

---
+++ src/main/java/br/com/forum/api_forum_alura/ApiForumAluraApplication.java
@@ -0,0 +1,13 @@
+package br.com.forum.api_forum_alura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiForumAluraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiForumAluraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/forum/api_forum_alura/ApiForumAluraApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.forum.api_forum_alura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiForumAluraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
